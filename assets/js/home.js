044a8cc706979d0c89ded9ecb7a8e46decb96f28
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-03-19 00:19:38 +0100

    did assignment on showing the popup when the nav-logo is clicked; did assignment on using if else statement to check if username and password is correct; worked on the scroll to top button - made it to display only after scrolling a certain height and making the page to scroll to the top when it is clicked

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,83 +1,45 @@
-let popup = document.querySelector("#popup")
-let close = document.querySelector("#close")
+let popup = document.querySelector("#popup");
+let close = document.querySelector("#close");
+var logo = document.querySelector("#logo")
 
-close.addEventListener('click', () => {
-    popup.style.display = 'none';
+logo.addEventListener("click", () => {
+  popup.style.display = "block";
 })
 
-
-
-// if (5 > 6) {
-//     console.log("5 is the greatest")
-// } else {
-//     console.log("5 is not the greatest")
-// }
-
-// var num1 = 17
-// var num2 = 12
-
-// if (num1 > num2) {
-//     console.log("num1 is greater than num2")
-//     console.log(num1 + " is greater than " + num2)
-// } else {
-//     console.log("num2 is greater than num2")
-//     console.log(num2 + " is greater than " + num1)
-// }
-
-
-// var message = "How are you? "
-
-// if (message == "hi") {
-//     console.log("hello")
-// } else if (message == "I am fine") {
-//     console.log("I am fine too")
-// } else if (message == "I need your help") {
-//     console.log("I no dey help")
-// } else {
-//     console.log("I don't understand you")
-// }
-
-
-var level = "JSS3"
-var age = 19
-var grade = 80
-
-// if (level == "JSS1") {
-//     if (age <= 18) {
-//         if (grade >= 70) {
-//             console.log("You are eligible to apply")
-//         } else {
-//             console.log("Your grade is below the pass mark")
-//         }
-//     } else {
-//         console.log("Your age is not allowed to apply")
-//     }
-// } else {
-//     console.log("Your level is not allowed")
-// }
-
-if (level == "JSS1" && age <= 18 && grade >= 70) {
-    console.log("You are allowed")
-} else {
-    console.log("Your level, age or grade is not allowed")
-}
-
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
 
 // Assignment
-// write a program that checks the username and password of a user 
-var username = "Uche"
-var password = "mynameisuche"
+// write a program that checks the username and password of a user
+var username = "Uche";
+var password = "mynameisuche";
 
 if (username === "Uche") {
-    if (password === "mynameisuche") {
-        console.log("You're successfully logged in")
-    } else {
-        console.log("Username or password incorrect")
-    }
+  if (password === "mynameisuche") {
+    console.log("You're successfully logged in");
+  } else {
+    console.log("Username or password incorrect");
+  }
 }
 
 if (username === "Uche" && password === "mynameisuche") {
-    console.log("You're successfully logged in")
+  console.log("You're successfully logged in");
 } else {
-    console.log("Username or password incorrect")
+  console.log("Username or password incorrect");
 }
+
+
+const toUp = document.querySelector("#to-up");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toUp.style.display = "flex";
+  } else {
+    toUp.style.display = "none";
+  }
+});
+
+toUp.addEventListener("click", () => {
+  window.scrollTo(0, 0)
+});
